808c97a268e97462ea02676a08bae684a4edf374
Bookoostrikes <bookoostrikes77@gmail.com>
2025-06-15 11:52:11 -0400

    Add files via upload

---
+++ JS/RocketFAQScript.js
@@ -0,0 +1,39 @@
+﻿const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+
+const FAQ1 = document.querySelector('.FAQ1');
+const Q1 = document.querySelector(`#Q1`);
+
+const FAQ2= document.querySelector('.FAQ2');
+const Q2 = document.querySelector(`#Q2`);
+
+const Q1ID = document.getElementById('Q1');
+const Q2ID = document.getElementById('Q2');
+
+
+
+Q1.addEventListener(`click`, event => {
+
+    FAQ1.classList.toggle('ON');
+    Q1ID.classList.toggle('TURN');
+});
+
+Q2.addEventListener(`click`, event => {
+
+    FAQ2.classList.toggle('ON');
+    Q2ID.classList.toggle('TURN');
+
+
+
+  
+});
+
+
+
+
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
---
+++ JS/RocketSignUp.js
@@ -0,0 +1,25 @@
+﻿const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+const no = document.querySelector('#no');
+const yes = document.querySelector('#yes');
+const warning = document.querySelector('.warning');
+
+
+no.addEventListener('click', function () {
+    if (no.checked) {
+        warning.style.display = "flex";
+    }
+});
+
+yes.addEventListener('click', function () {
+    if (yes.checked) {
+        warning.style.display = "none";
+    }
+});
+
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
